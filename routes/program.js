2ddbd651382576402eac0fdfe3153e9b5dacbb51
noidontdig <alexandraqin@gmail.com>
2018-02-23 17:49:34 -0500

    Fix routing

--- routes/program.js
+++ routes/program.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET program page. */
 router.get('/program', function(req, res, next) {
-    res.render('program', { title: 'our program' });
+  console.log('hi');
+  res.render('program', { title: 'our program' });
 });
 
 module.exports = router;
