0a7356daa4159cc83a74dff2fe97cd060e47ad8e
Adekunle Oduye <adekunleoduye@gmail.com>
2019-12-01 09:03:22 -0500

    [nav] mobile structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import './header.scss'
+import logo from '../../_assets/images/logo--horizontal.svg'
 
-export const Header = () => (
-  <header className="mainHeader">
-    <div className="mainHeader-logoGroup">Logo</div>
-    <nav className="mainHeader-primaryNav">
-      <ul className="primaryNav-linkGroup">
-        <li className="primaryNav-linkItem">
-          <Link to="#">About us</Link>
-        </li>
-        <li className="primaryNav-linkItem">
-          <Link to="#">Volunteer</Link>
-        </li>
-        <li className="primaryNav-linkItem">
-          <Link to="#">Code</Link>
-        </li>
-        <li className="primaryNav-linkItem">
-          <Link to="#">Donate</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+export const Header = () => {
+  const [navVisibility, setNavVisibility] = useState(false)
+  const toggleVisibility = navVisibility ? 'is-visible' : 'is-hidden'
+  const activeMenu = e => {
+    setNavVisibility(!navVisibility)
+  }
+
+  function handleChange(newValue) {
+    setNavVisibility(newValue)
+  }
+  return (
+    <header className="mainHeader content">
+      <div className="mainHeader-logoGroup">
+        <img className="logo--outline" src={logo} alt="the code cooperative" />
+      </div>
+      <button
+        type="button"
+        className={`mainHeader-navIcon ${navVisibility ? 'is-active' : ''}`}
+        onClick={() => activeMenu()}
+      >
+        <div></div>
+      </button>
+      <nav
+        className={`mainHeader-primaryNav ${navVisibility ? 'is-active' : ''}`}
+      >
+        <ul className="primaryNav-linkGroup">
+          <li className="primaryNav-linkItem">
+            <Link className="primaryNav-link" to="#">
+              About us
+            </Link>
+          </li>
+          <li className="primaryNav-linkItem">
+            <Link className="primaryNav-link" to="#">
+              Volunteer
+            </Link>
+          </li>
+          <li className="primaryNav-linkItem">
+            <Link className="primaryNav-link" to="#">
+              Contact
+            </Link>
+          </li>
+          <li className="primaryNav-linkItem">
+            <Link className="btn btn-tertiary primaryNav-link" to="#">
+              Donate
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
