a637875c050f67a44469fa6e34b5c268ab9fd1e8
Adekunle Oduye <adekunleoduye@gmail.com>
2019-12-01 09:24:55 -0500

    [nav] completed desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,43 +14,51 @@ export const Header = () => {
     setNavVisibility(newValue)
   }
   return (
-    <header className="mainHeader content">
-      <div className="mainHeader-logoGroup">
-        <img className="logo--outline" src={logo} alt="the code cooperative" />
+    <header className="mainHeader">
+      <div className="mainHeader-inner content">
+        <div className="mainHeader-logoGroup">
+          <img
+            className="logo--outline"
+            src={logo}
+            alt="the code cooperative"
+          />
+        </div>
+        <button
+          type="button"
+          className={`mainHeader-navIcon ${navVisibility ? 'is-active' : ''}`}
+          onClick={() => activeMenu()}
+        >
+          <div></div>
+        </button>
+        <nav
+          className={`mainHeader-primaryNav ${
+            navVisibility ? 'is-active' : ''
+          }`}
+        >
+          <ul className="primaryNav-linkGroup">
+            <li className="primaryNav-linkItem">
+              <Link className="primaryNav-link" to="#">
+                About us
+              </Link>
+            </li>
+            <li className="primaryNav-linkItem">
+              <Link className="primaryNav-link" to="#">
+                Volunteer
+              </Link>
+            </li>
+            <li className="primaryNav-linkItem">
+              <Link className="primaryNav-link" to="#">
+                Contact
+              </Link>
+            </li>
+            <li className="primaryNav-linkItem">
+              <Link className="btn btn-tertiary primaryNav-link" to="#">
+                Donate
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <button
-        type="button"
-        className={`mainHeader-navIcon ${navVisibility ? 'is-active' : ''}`}
-        onClick={() => activeMenu()}
-      >
-        <div></div>
-      </button>
-      <nav
-        className={`mainHeader-primaryNav ${navVisibility ? 'is-active' : ''}`}
-      >
-        <ul className="primaryNav-linkGroup">
-          <li className="primaryNav-linkItem">
-            <Link className="primaryNav-link" to="#">
-              About us
-            </Link>
-          </li>
-          <li className="primaryNav-linkItem">
-            <Link className="primaryNav-link" to="#">
-              Volunteer
-            </Link>
-          </li>
-          <li className="primaryNav-linkItem">
-            <Link className="primaryNav-link" to="#">
-              Contact
-            </Link>
-          </li>
-          <li className="primaryNav-linkItem">
-            <Link className="btn btn-tertiary primaryNav-link" to="#">
-              Donate
-            </Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   )
 }
