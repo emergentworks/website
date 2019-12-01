5890c136bfe6f6f14429d5a12a8f96431e16960a
Adekunle Oduye <adekunleoduye@gmail.com>
2019-11-30 23:32:53 -0500

    [infra] Init Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,41 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Header from '../Header'
+import Footer from '../Footer'
+import './layout.scss'
+
+export const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <div>
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
