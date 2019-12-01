6e788ac92942b29bf7495637c715b824f002b753
Adekunle Oduye <adekunleoduye@gmail.com>
2019-11-30 23:33:39 -0500

    [infra] update header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,24 @@
 import React from 'react'
-// import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 
 export const Header = () => (
   <header className="mainHeader">
     <div className="mainHeader-logoGroup">Logo</div>
-    {/* <nav className="mainHeader-primaryNav">
+    <nav className="mainHeader-primaryNav">
       <ul className="primaryNav-linkGroup">
         <li className="primaryNav-linkItem">
           <Link to="#">About us</Link>
         </li>
         <li className="primaryNav-linkItem">
-          <Link to="#">About us</Link>
+          <Link to="#">Volunteer</Link>
         </li>
         <li className="primaryNav-linkItem">
-          <Link to="#">About us</Link>
+          <Link to="#">Code</Link>
         </li>
         <li className="primaryNav-linkItem">
-          <Link to="#">About us</Link>
+          <Link to="#">Donate</Link>
         </li>
       </ul>
-    </nav> */}
+    </nav>
   </header>
 )
