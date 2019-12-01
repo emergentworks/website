309f4f74ac08d5010d36d81ddce83e8a5c7bab0f
Adekunle Oduye <adekunleoduye@gmail.com>
2019-11-30 23:19:20 -0500

    [infra] Init Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+export const Header = () => (
+  <header className="mainHeader">
+    <div className="mainHeader-logoGroup">Logo</div>
+    {/* <nav className="mainHeader-primaryNav">
+      <ul className="primaryNav-linkGroup">
+        <li className="primaryNav-linkItem">
+          <Link to="#">About us</Link>
+        </li>
+        <li className="primaryNav-linkItem">
+          <Link to="#">About us</Link>
+        </li>
+        <li className="primaryNav-linkItem">
+          <Link to="#">About us</Link>
+        </li>
+        <li className="primaryNav-linkItem">
+          <Link to="#">About us</Link>
+        </li>
+      </ul>
+    </nav> */}
+  </header>
+)
