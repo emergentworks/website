c675ef7b19e141fc82bbb4ed265f334777352fdf
Adekunle Oduye <adekunleoduye@gmail.com>
2019-12-02 09:15:04 -0500

    finished home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout'
 // import Image from "../components/image"
 import SEO from '../components/seo'
 import logo from '../_assets/images/logo--stacked.svg'
+import './index.scss'
 
 const IndexPage = () => (
   <Layout className="home">
@@ -17,6 +18,7 @@ const IndexPage = () => (
             src={logo}
             alt="The code cooperative logo"
           />
+          {/* <div className="heroLogo-shape"></div> */}
         </div>
         <div className="heroContent-group">
           <p className="heroLogo-content">
@@ -24,10 +26,36 @@ const IndexPage = () => (
             build technology to create life changing possibilities for
             individuals and communities impacted by incarceration.
           </p>
-          <Link to="/about" className="btn btn-primary">
+          <Link to="/about" className="btn btn-primary w-100">
             Learn More
           </Link>
         </div>
+      </div>
+    </section>
+    <section className="homepage-cardGroup content">
+      <div className="card--photoCard photoCard--card-1">
+        <h2 className="card-title">About</h2>
+        <p className="card-content">
+          We believe that providing access to computer science education to the
+          most marginalized communities in our society.
+        </p>
+        <Link className="card-cta btn btn-secondary w-100">Learn More</Link>
+      </div>
+      <div className="card--photoCard photoCard--card-2">
+        <h2 className="card-title">Volunteer</h2>
+        <p className="card-content">
+          Our mentors work with fellows to meet their learning goals in computer
+          literacy, code, and web development.
+        </p>
+        <Link className="card-cta btn btn-secondary w-100">Learn More</Link>
+      </div>
+      <div className="card--photoCard photoCard--card-3">
+        <h2 className="card-title">Donate</h2>
+        <p className="card-content">
+          Our program runs on donations so that we can provide Metrocards, food,
+          hardware and office supplies for our fellows.
+        </p>
+        <Link className="card-cta btn btn-secondary w-100">Donate Now</Link>
       </div>
     </section>
   </Layout>
