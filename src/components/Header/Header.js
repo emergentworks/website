222158f0bb1d8e3a4b77fd84370368dcf0509216
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-07 23:28:07 -0400

    [rebrand] Init Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import cx from 'classnames'
 
+import Nav from 'components/Nav'
 import logo from '../../_assets/images/logo--horizontal.svg'
 
 import styles from './Header.module.scss'
@@ -15,7 +16,25 @@ export const Header = () => {
 
   return (
     <header className={cx(styles.root, 'content')}>
-      <div className={styles.container}>Header</div>
+      <div className={styles.container}>
+        <div className={styles.logoGroup}>
+          <Link to="/">
+            <img className={styles.logo} src={logo} alt="emergent works" />
+          </Link>
+        </div>
+        <div className={styles.navGroup}>
+          <button
+            type="button"
+            className={cx(styles.navButton, {
+              [styles.isActive]: navVisibility,
+            })}
+            onClick={() => activeMenu()}
+          >
+            <div></div>
+          </button>
+          <Nav className={styles.nav} navVisibility={navVisibility} />
+        </div>
+      </div>
     </header>
   )
 }
