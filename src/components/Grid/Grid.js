5959453775092f86b695a9788711baf19651e17a
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-07 23:27:50 -0400

    [rebrand] init Grid

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+
+import styles from './Grid.module.scss'
+
+export const Grid = ({
+  children,
+  className,
+  gap = 2,
+  col = false,
+  ...rest
+}) => (
+  <div
+    className={cx(styles.root, className, {
+      [styles[`gap${gap}`]]: gap,
+      [styles[`col${col}`]]: col,
+      [styles.columns]: col,
+    })}
+    {...rest}
+  >
+    {children}
+  </div>
+)
+export const GridItem = ({ children, className, ...rest }) => (
+  <div className={cx(styles.item, className)} {...rest}>
+    {children}
+  </div>
+)
+
+Grid.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  gap: PropTypes.number,
+  col: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+}
+
+GridItem.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+}
