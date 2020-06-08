b89cff9d4ff135a117ed628b48865edd94ca5c8f
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-07 23:26:11 -0400

    [rebrand] Webpackcnfig for absolute imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+// Absolute imports paths
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  })
+}
