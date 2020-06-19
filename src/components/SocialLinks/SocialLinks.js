2b14de5135383f8af84a35460044deb8c08e6e35
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-19 12:31:00 -0400

    [rebrand] SocialLinks: Cleanup

--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -4,33 +4,27 @@ import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 import Icon from 'components/Icon'
-import twitterLogo from '../../_assets/images/icon-twitter.svg'
-import instagramLogo from '../../_assets/images/icon-instagram.svg'
 
 import styles from './SocialLinks.module.scss'
 
-export const SocialLinks = ({ className, ...rest }) => {
-  const getYear = new Date().getFullYear()
-
-  return (
-    <div className={cx(styles.root, className)} {...rest}>
-      <Link to="/" className={styles.link}>
-        <Icon
-          className={styles.icon}
-          type="instagram"
-          title="Follow Emergent Works on Instagram"
-        />
-      </Link>
-      <Link to="/" className={styles.link}>
-        <Icon
-          className={styles.icon}
-          type="twitter"
-          alt="Follow Emergent Works on Twitter"
-        />
-      </Link>
-    </div>
-  )
-}
+export const SocialLinks = ({ className, ...rest }) => (
+  <div className={cx(styles.root, className)} {...rest}>
+    <Link to="/" className={styles.link}>
+      <Icon
+        className={styles.icon}
+        type="instagram"
+        title="Follow Emergent Works on Instagram"
+      />
+    </Link>
+    <Link to="/" className={styles.link}>
+      <Icon
+        className={styles.icon}
+        type="twitter"
+        alt="Follow Emergent Works on Twitter"
+      />
+    </Link>
+  </div>
+)
 
 SocialLinks.propTypes = {
   className: PropTypes.string,
