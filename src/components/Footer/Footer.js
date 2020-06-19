dd4fa6e11de07433d49c1fb2d755a3942639ef26
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-19 12:31:35 -0400

    [rebrand] Footer: added copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,22 +6,28 @@ import SocialLinks from 'components/SocialLinks'
 
 import styles from './Footer.module.scss'
 
-export const Footer = () => (
-  <footer className={cx(styles.root)}>
-    <div className={cx(styles.container, 'content')}>
-      <div className={styles.linkGroup}>
-        <div className={styles.navGroup}>
-          <Nav className={styles.nav} showHomeLink showHomeEmail />
+export const Footer = () => {
+  const getYear = new Date().getFullYear()
+
+  return (
+    <footer className={cx(styles.root)}>
+      <div className={cx(styles.container, 'content')}>
+        <div className={styles.linkGroup}>
+          <div className={styles.navGroup}>
+            <Nav className={styles.nav} showHomeLink showHomeEmail />
+          </div>
+          <div className={styles.socialGroup}>
+            <SocialLinks />
+          </div>
         </div>
-        <div className={styles.socialGroup}>
-          <SocialLinks />
-        </div>
+        <p className={styles.description}>
+          Emergent Works is a fiscally sponsored program of Prime Produce
+          Limited. Prime Produce Limited is a registered 501(c)(3) corporation,
+          and all donations are tax deductible to the extent permitted by law.
+        </p>
+
+        <p className={styles.description}>© {getYear} Emergent Works</p>
       </div>
-      <p className={styles.description}>
-        Emergent Works is a fiscally sponsored program of Prime Produce Limited.
-        Prime Produce Limited is a registered 501(c)(3) corporation, and all
-        donations are tax deductible to the extent permitted by law.
-      </p>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
