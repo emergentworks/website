b71ae8b88048f2fc63e019b5bc9a4801fa8d11a3
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-25 07:06:27 -0400

    [rebrand] Button: Extended with Link

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,26 +1,53 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import { Link } from 'gatsby'
 
 import Icon from 'components/Icon'
 
 import styles from './Button.module.scss'
 
-export const Button = ({ children, className, type = 'primary', ...rest }) => (
-  <button
-    type="button"
-    className={cx(styles.root, className, { [styles[type]]: type })}
-    {...rest}
-  >
-    <span className={styles.labelGroup}>{children}</span>
-    <span className={styles.iconGroup}>
-      <Icon type="arrowRight" />
-    </span>
-  </button>
-)
+export const Button = ({
+  children,
+  className,
+  to,
+  onClick,
+  type = 'primary',
+  ...rest
+}) => {
+  if (to && !onClick) {
+    return (
+      <Link
+        className={cx(styles.root, className, { [styles[type]]: type })}
+        to={to}
+        {...rest}
+      >
+        <span className={styles.labelGroup}>{children}</span>
+        <span className={styles.iconGroup}>
+          <Icon type="arrowRight" />
+        </span>
+      </Link>
+    )
+  }
+  return (
+    <button
+      type="button"
+      className={cx(styles.root, className, { [styles[type]]: type })}
+      {...rest}
+      onClick={onClick}
+    >
+      <span className={styles.labelGroup}>{children}</span>
+      <span className={styles.iconGroup}>
+        <Icon type="arrowRight" />
+      </span>
+    </button>
+  )
+}
 
 Button.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   type: PropTypes.oneOf(['primary']),
+  to: PropTypes.string,
+  onClick: PropTypes.func,
 }
