226aea2b30c0f1603d592303add58cc5f22a72a0
Adekunle Oduye <adekunleoduye@gmail.com>
2020-08-22 20:03:46 -0400

    [rebrand] Add new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/_assets/images/favicon.svg`, // This path is relative to the root of the site.
+        icon: `src/_assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // Setting up file nodes for news
--- gatsby-node.js
+++ gatsby-node.js
@@ -25,7 +25,6 @@ exports.createPages = ({ actions, graphql }) => {
               blurb
             }
             body
-            html
             id
           }
         }
