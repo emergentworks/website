8dd8fa42786af7fb71217a3418723dcafccaeb8d
noidontdig <alexandraqin@gmail.com>
2020-08-26 14:48:58 -0400

    Replace agency page with deck

--- src/pages/for-clients.js
+++ src/pages/for-clients.js
@@ -13,67 +13,93 @@ import styles from './for-clients.module.scss'
 
 const ForClientsPage = ({ data }) => {
   // eslint-disable-next-line react/prop-types
-  const ImgShowcase = data.ImgShowcase.childImageSharp.fluid
+  const Img1 = data.Img1.childImageSharp.fluid
+  const Img2 = data.Img2.childImageSharp.fluid
+  const Img3 = data.Img3.childImageSharp.fluid
+  const Img4 = data.Img4.childImageSharp.fluid
+  const Img5 = data.Img5.childImageSharp.fluid
+  const Img6 = data.Img6.childImageSharp.fluid
+  const Img7 = data.Img7.childImageSharp.fluid
+  const Img8 = data.Img8.childImageSharp.fluid
+  const Img9 = data.Img9.childImageSharp.fluid
+  const Img10 = data.Img10.childImageSharp.fluid
+  const Img11 = data.Img11.childImageSharp.fluid
+
 
   return (
     <Layout className={styles.page}>
       <SEO title="For Clients" />
       <div className={cx(styles.root, 'content')}>
         <h1 className={cx(styles.title, 'content-max-width')}>
-          Emergent Works builds software for purpose-driven
+          Emergent Works builds software for purposeful
           organizations while creating employment opportunities for formerly incarcerated people.
         </h1>
-        <ImgCaption
-          fluid={ImgShowcase}
-          alt="2020 Fellowship Showcase"
-          caption="2020 Fellowship Showcase"
-        />
-        <Grid col={2} gap={4}>
-          <GridItem>
-            <section className={styles.servicesGroup}>
-              <div className={styles.servicesItem}>
-                <h2>Full stack web products</h2>
-                <p>
-                  Our team of web developers can deliver anything from a fully
-                  bespoke website, to Squarespace customizations, to internal
-                  tooling.
-                </p>
-              </div>
-              <div className={styles.servicesItem}>
-                <h2>Impact-driven work</h2>
-                <p>
-                  Experienced product leads guarantee that teams not only
-                  delivery the technology you want, but the business outcomes
-                  you need.
-                </p>
-              </div>
-              <div className={styles.servicesItem}>
-                <h2>Investment in your community</h2>
-                <p>
-                  Through our projects, our formerly incarcerated apprentices
-                  get valuable, paid work experience to kickstart their careers
-                  in tech.
-                </p>
-              </div>
-            </section>
-          </GridItem>
-          <GridItem align="middle" justify="center">
-            <WaterColor alt="" />
-          </GridItem>
-        </Grid>
-        <section className={styles.contactUs}>
-          <p className="h2">
-            Let’s work together. Whether you have a project in mind, or need
-            help defining it, we'd love to collaborate with you.
-          </p>
-          <p className="h2">
-            Contact us at{' '}
-            <a href="mailto:agency@emergentworks.org?subject=[Action] Emergent Works Agency">
-              agency@emergentworks.org
-            </a>
-            .
-          </p>
-        </section>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img1}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img2}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img3}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img4}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img5}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img6}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img7}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img8}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img9}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img10}
+          />
+        </div>
+        <div className={styles.work}>
+          <ImgCaption
+            className={styles.work}
+            fluid={Img11}
+          />
+        </div>
       </div>
     </Layout>
   )
@@ -81,7 +107,87 @@ const ForClientsPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    ImgShowcase: file(relativePath: { eq: "showcase-2020-1.jpg" }) {
+    Img1: file(relativePath: { eq: "agency/agency1.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img2: file(relativePath: { eq: "agency/agency2.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img3: file(relativePath: { eq: "agency/agency3.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img4: file(relativePath: { eq: "agency/agency4.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img5: file(relativePath: { eq: "agency/agency5.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img6: file(relativePath: { eq: "agency/agency6.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img7: file(relativePath: { eq: "agency/agency7.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img8: file(relativePath: { eq: "agency/agency8.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img9: file(relativePath: { eq: "agency/agency9.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img10: file(relativePath: { eq: "agency/agency10.png" }) {
+      childImageSharp {
+        id
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    Img11: file(relativePath: { eq: "agency/agency11.png" }) {
       childImageSharp {
         id
         fluid {
