8204852dc4dbb3e3ff7ac3623905cebafce5c697
Adekunle Oduye <adekunleoduye@gmail.com>
2020-10-02 13:10:56 -0400

    [lead_capture] feat: updated button and form components

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -1,38 +1,68 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import { BiErrorCircle } from 'react-icons/bi'
+
 import { generateUUID } from '../../helpers/dom'
 import styles from './FormField.module.scss'
 
-export const FormField = ({
-  value = '',
-  label = '',
-  type = 'text',
-  onChange = () => {},
-  ...rest
-}) => {
+export const FormField = React.forwardRef(function FormField(
+  {
+    value = '',
+    label = '',
+    type = 'text',
+    errorMessage = '',
+    className,
+    onChange,
+    ...rest
+  },
+  forwardRef
+) {
   const [uuid] = useState(generateUUID())
-  const textDescLocationClass = value ? styles.textHasValue : ''
 
   return (
-    <div htmlFor={uuid} className={cx(styles.root)}>
-      <input
-        className={cx(styles.input)}
-        type={type}
-        value={value}
-        id={uuid}
-        onChange={e => onChange(e.target.value)}
-        placeholder={label}
-        aria-label={label}
-        {...rest}
-      ></input>
+    <div htmlFor={uuid} className={cx(styles.root, className)}>
+      {type === 'textarea' ? (
+        <textarea
+          className={cx(styles.input)}
+          type={type}
+          value={value}
+          id={uuid}
+          ref={forwardRef}
+          onChange={onChange}
+          placeholder={label}
+          aria-label={label}
+          {...rest}
+        />
+      ) : (
+        <input
+          className={cx(styles.input)}
+          type={type}
+          value={value}
+          id={uuid}
+          ref={forwardRef}
+          onChange={onChange}
+          placeholder={label}
+          aria-label={label}
+          {...rest}
+        />
+      )}
+
+      {errorMessage && (
+        <div className={styles.errorContainer}>
+          <BiErrorCircle className={styles.errorIcon} />
+          <span className={styles.errorMessage}>{errorMessage}</span>
+        </div>
+      )}
     </div>
   )
-}
+})
 
 FormField.propTypes = {
+  errorMessage: PropTypes.string,
+  className: PropTypes.string,
   value: PropTypes.string,
   label: PropTypes.string,
-  type: PropTypes.string,
+  type: PropTypes.oneOf('text', 'textarea'),
   onChange: PropTypes.func,
 }
