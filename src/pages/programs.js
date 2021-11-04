97da5cacf42bff51e713d405256c291fb4a2e7b9
Mark Marcantano <m.marcantano@gmail.com>
2021-11-04 10:12:50 -0700

    make copy fixes on the programs page

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -25,10 +25,10 @@ const ProgramsPage = () => (
           ></iframe>
         </div>
         <h2 className={cx(styles.subTitle, 'content-max-width')}>
-          Technical Mentorship Program
+          1:1 Virtual Technical Mentorship Program
         </h2>
         <p className="content-max-width">
-          The Emergent Works Technical Mentorship Program provides returning citizens with exposure to technology and digital literacy training. During the program, mentees are paired with mentors who are currently working as software engineers in the tech sector. Mentees first work to develop the digital literacy skills necessary to learn software development fundamentals.  Mentees then work to conceptualize, design, and implement a software development project under the guidance of, and in collaboration with, their mentors. During the culminating showcase event of the program, pairs present their completed projects to the EW community. The EW mentorship program serves as a gateway to careers in software engineering by providing our community members with access and exposure to a supportive community of learners and professionals, educational resources, and a professional network of industry-leading technologists.
+          The Emergent Works 1:1 Virtual Technical Mentorship Program provides returning citizens with exposure to technology and digital literacy training. During the program, mentees are paired with mentors who are currently working as software engineers in the tech sector. Mentees first work to develop the digital literacy skills necessary to learn software development fundamentals.  Mentees then work to conceptualize, design, and implement a software development project under the guidance of, and in collaboration with, their mentors. During the culminating showcase event of the program, pairs present their completed projects to the EW community. The EW mentorship program serves as a gateway to careers in software engineering by providing our community members with access and exposure to a supportive community of learners and professionals, educational resources, and a professional network of industry-leading technologists.
         </p>
         <div>
           <Button
@@ -60,17 +60,17 @@ const ProgramsPage = () => (
 
       <section className={cx(styles.section, styles.mentorshipSection)}>
         <h2 className={cx(styles.subTitle, 'content-max-width')}>
-          Technology Rhythm And Passion Program (TRAP)
+          Technology Rhythm And Passion Program (T.RAP)
         </h2>
         <p className="content-max-width">
-          The Emergent Works Technical Mentorship Program provides returning citizens with exposure to technology and digital literacy training. During the program, mentees are paired with mentors who are currently working as software engineers in the tech sector. Mentees first work to develop the digital literacy skills necessary to learn software development fundamentals.  Mentees then work to conceptualize, design, and implement a software development project under the guidance of, and in collaboration with, their mentors. During the culminating showcase event of the program, pairs present their completed projects to the EW community. The EW mentorship program serves as a gateway to careers in software engineering by providing our community members with access and exposure to a supportive community of learners and professionals, educational resources, and a professional network of industry-leading technologists.
+          Emergent Work’s Technology Rhythm And Passion is a 16-week program that introduces artists from communities impacted by incarceration to technology through therapeutic writing practices, music/video production, and audio engineering. The program provides participants with a fundamental working-knowledge of DAW’s, music production, and song writing. TRAP teaches participants the requisite web development skills to build a web page that showcases the skills they gain recording with professional audio engineers and learning the practices of therapeutic songwriting. The program culminates with participants conceptualizing, designing, and developing a website to showcase the music they create during the program which is informed by these practices.
         </p>
         <div>
           <Button
             className="mt--lg"
             href="https://docs.google.com/forms/d/e/1FAIpQLSfLyOvsMCU1wnldsvWNjIgWaDYOysT9Gc-ITsRzCyU4K1g8dQ/viewform"
           >
-            Apply to become a TRAP mentee
+            Apply to become a T.RAP mentee
           </Button>
         </div>
       </section>
