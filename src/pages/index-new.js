045f7804cd60b27425641ba1c1465289a0010eb1
Ariel Herman <aeh.herman@gmail.com>
2022-11-07 14:10:51 -0500

    WIP add card styles

--- src/pages/index-new.js
+++ src/pages/index-new.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
-
 import cx from 'classnames';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
-
 import styles from './index-new.module.scss';
 
 const IndexPage2 = () => {
   return (
     <Layout className="">
       <SEO title="Home" />
-      {/* <section className={cx(styles.heroWrapper)}>
-        <div>
+      <section className={cx(styles.heroWrapper)}>
+        <div className={cx(styles.heroCopy)}>
           <h1 className="">We are Emergent Works.</h1>
+          <p>
+            "When I was finally released from my 9.5 year prison sentence, many
+            employers would not hire me. I knew I wanted to become a software
+            engineer but I needed extra help. The dedication, time, resources,
+            equipment, emotional support and financial support from Emergent
+            Works is the biggest reason I was able to see this through. Today I
+            can proudly say I am a thriving Software Engineer." - Makeda, EW
+            2020 Winter Mente
+          </p>
         </div>
-
-        <StaticImage
-          src="..src/_assets/images/team-core/placeholder.png"
-          alt="Members of the Emergent Works team"
-            aspectRatio={1}
-          layout="constrained"
-            cropFocus="attention"
-          className={cx(styles.heroImage)}
-        />
-      </section> */}
+        <div className={cx(styles.heroImage)}>
+          <StaticImage
+            src="../_assets/images/home-page-grid/grant-announcement.png"
+            alt="Members of the Emergent Works team"
+          />
+        </div>
+      </section>
       <section className={cx(styles.threeUpCardWrapper)}>
         <div>
           <StaticImage
-            src="..src/_assets/images/team-core/placeholder.png"
+            src="../_assets/images/team-core/placeholder.png"
             alt="Members of the Emergent Works team"
             aspectRatio={1}
             layout="constrained"
-            cropFocus="attention"
+            // cropFocus="attention"
             className={cx(styles.cardImageRound)}
           />
           <span>
@@ -71,7 +76,7 @@ const IndexPage2 = () => {
             alt="Members of the Emergent Works team"
             aspectRatio={1}
             layout="constrained"
-            cropFocus="attention"
+            // cropFocus="attention"
             className={cx(styles.cardImageRound)}
           />
           <span>
@@ -84,6 +89,22 @@ const IndexPage2 = () => {
             full-time job with The Walt Disney Company. Awesome program!
           </span>
         </div>
+      </section>
+      <section>
+        <ul className={cx(styles.twoUpCardWrapper)}>
+          <li className={styles.twoUpCard}>
+            <Link to="/programs">
+              <h3>Our Programs</h3>
+              <span>Learn or teach with us</span>
+            </Link>
+          </li>
+          <li className={styles.twoUpCard}>
+            <Link to="/products">
+              <h3>Our Projects</h3>
+              <span>We build tech to drive change</span>
+            </Link>
+          </li>
+        </ul>
       </section>
     </Layout>
   );
