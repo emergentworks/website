0a38206c3144fc285c2891d88438178b825ba5b8
Ariel Herman <aeh.herman@gmail.com>
2023-09-25 16:39:57 -0400

    MVP of programs page

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,12 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 import Layout from 'components/Layout';
 import SEO from 'components/seo';
 import { graphql, useStaticQuery } from 'gatsby';
-import { getImage } from 'gatsby-plugin-image';
+import { getImage, StaticImage } from 'gatsby-plugin-image';
+import CtaLink from '../components/CtaLink/CtaLink';
 
 import styles from './programs.module.scss';
 import { Hero } from '../components/Hero/Hero';
+
+const ProgramCard = ({
+  title,
+  qualifications,
+  commitments,
+  description,
+  links,
+}) => {
+  return (
+    <div className={cx(styles.programCard, 'twoUp mb')}>
+      <div className="twoUpCard">
+        <h3 className={cx(styles.programTitle)}>{title}</h3>
+      </div>
+      <div className={cx(styles.programInfo, 'twoUpCard')}>
+        {qualifications && (
+          <ul aria-label="Qualifications">
+            {qualifications.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        )}
+        {commitments && (
+          <ul aria-label="Commitment">
+            {commitments.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        )}
+        <div>
+          <p>{description}</p>
+          <div>
+            {links ? (
+              links.map((link) => (
+                <CtaLink href={link.href}>{link.title}</CtaLink>
+              ))
+            ) : (
+              <span>More information coming soon!</span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ProgramsPage = () => {
   const data = useStaticQuery(graphql`
@@ -16,47 +62,77 @@ const ProgramsPage = () => {
           gatsbyImageData(aspectRatio: 3, layout: FULL_WIDTH)
         }
       }
+      programs: allProgramsJson {
+        nodes {
+          description
+          commitments
+          links {
+            href
+            title
+          }
+          qualifications
+          title
+        }
+      }
     }
   `);
+
+  const programs = data.programs.nodes;
+
   return (
     <Layout className={styles.page}>
       <SEO title="Programs" />
       <div className={cx(styles.root)}>
         <Hero image={getImage(data.hero)} title="Our Programs" />
         <section>
-          <p>
+          <p className={cx(styles.prominent, 'tc')}>
             We imagine a world where technical education unlocks equitable
             economic mobility for system impacted communities
           </p>
           <div className="twoUp">
-            {/* <div className="twoUpCard"></div> */}
+            <div className="twoUpCard">
+              <StaticImage
+                src="../_assets/images/programs/programming.jpg"
+                alt="Mentorship Program participants"
+                cropFocus="attention"
+              />
+            </div>
             <div className="twoUpCard">
               <h2>Technical Education Programming</h2>
               <p>
-                Emergent Works trains formerly incarcerated and those impacted
-                by the criminal legal system through technical programming that
-                provides free access to education, resources, a safe community,
-                and employment opportunities; in hopes of ending the revolving
-                door of mass incarceration and closing the wealth gap and
-                digital divide.
+                Emergent Works trains individuals who were formerly-incarcerated
+                and those impacted by the criminal legal system through
+                technical programming that provides free access to education,
+                resources, a safe community, and employment opportunities.
+                Through our programs, we hope to help end the revolving door of
+                mass incarceration by closing the wealth gap and digital divide.
               </p>
 
               <p>
-                Our three programs caters to different needs as well as various
-                learning levels.{' '}
+                Our three programs cater to different needs as well as various
+                learning levels.
               </p>
               <ul>
-                <li>In-person Program (BTTB)</li>
+                <li>In-person Program (Bigger Than The Block)</li>
                 <li>Virtual Program (1:1 Technical Mentorship)</li>
-                <li>Apprenticeship (LEAP) </li>
+                <li>Apprenticeship (Leadership Apprentice Program) </li>
               </ul>
             </div>
           </div>
         </section>
         <section>
           <h2>Explore our various programs </h2>
-          {/* <ProgramCard title="Bigger Than The Block" to=""></div> */}
-          {/* <div className="twoUpCard"></div> */}
+          {programs.map(
+            ({ title, qualifications, commitments, description, links }) => (
+              <ProgramCard
+                title={title}
+                qualifications={qualifications}
+                commitments={commitments}
+                description={description}
+                links={links}
+              />
+            )
+          )}
         </section>
       </div>
     </Layout>
@@ -64,3 +140,11 @@ const ProgramsPage = () => {
 };
 
 export default ProgramsPage;
+
+ProgramCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  qualifications: PropTypes.arrayOf(PropTypes.string),
+  commitments: PropTypes.arrayOf(PropTypes.string),
+  description: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.string),
+};
