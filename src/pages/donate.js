b1ae898fe3823b64bd27cc58690d80a5fce363fa
Ariel Herman <aeh.herman@gmail.com>
2023-09-26 15:11:04 -0400

    Update donate page

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,94 +1,83 @@
 import React from 'react';
 import cx from 'classnames';
-import { graphql } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
-
-import Grid, { GridItem } from 'components/Grid';
+import { graphql, useStaticQuery } from 'gatsby';
+import { getImage } from 'gatsby-plugin-image';
 import SEO from '../components/seo';
-import Button from '../components/Button';
 import Layout from '../components/Layout';
+import { Hero } from '../components/Hero/Hero';
 
 import styles from './donate.module.scss';
 
 const DonatePage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      hero: file(relativeDirectory: { eq: "hero" }, name: { eq: "donate" }) {
+        childImageSharp {
+          gatsbyImageData(aspectRatio: 3, layout: FULL_WIDTH)
+        }
+      }
+    }
+  `);
+
   return (
-    <Layout className={styles.page}>
+    <Layout>
       <SEO title="Donate" />
-      <div className={cx(styles.root, 'content')}>
-        <h1 className={cx('title', styles.pageTitle)}>
-          Emergent Works interrupts the prison cycle by creating pathways into
-          high-paying jobs in technology for formerly incarcerated people.
-        </h1>
-        <Grid gap={4}>
-          <Grid col={2} gap={4}>
-            <GridItem justify="left">
-              <h2 className="mt--none">Support our work</h2>
+      <div className={cx(styles.root)}>
+        <Hero image={getImage(data.hero)} title="Your Support" />
+        <section>
+          <p className={cx(styles.prominent, 'tc')}>
+            It only takes <span>$500</span> to support a mentee to gain
+            high-value technical skills in our technical mentorship program
+          </p>
+          <div className="twoUp">
+            <div className="twoUpCard">
               <p>
-                <strong>$50 donations</strong> pay for chargers for donated
-                laptops.
+                Learning is at the pillar of Emergent Works and we always want
+                to acknowledge the dedication each mentee takes to grow and
+                elevate their future. With your support, our mentees obtain
+                stipends while learning over the course of the program.
               </p>
-              <p>
-                <strong>$100 donations</strong> pay for guest speakers to teach
-                workshops.
-              </p>
-              <p>
-                <strong>$500 donations</strong> pay for learning stipends for
-                our participants.
-              </p>
-              <p>Anything you can give helps tremendously!</p>
-              <h2 className="mt--none">Donate laptops</h2>
-
-              <p>
-                All of our participants receive a laptop for the duration of the
-                program. Support our students’ learning by donating gently used
-                laptops!
-              </p>
-
-              <Button
-                className="mt--lg"
-                href="https://docs.google.com/forms/d/e/1FAIpQLSekRlaBYt0ILYZP90bJUiXfTf9fCl0brJwamNAgUoH1hIsZdg/viewform"
-              >
-                Donate laptops
-              </Button>
-              <StaticImage
-                src="../_assets/images/laptop.png"
-                alt="Donate laptops"
-                className={styles.laptop}
-                align="right"
-              />
-            </GridItem>
-            <GridItem align="center">
-              <script
-                src="https://donorbox.org/widget.js"
-                paypalExpress="false"
-              />
-              <iframe
-                title="Donate to Emergent Works!"
-                allowpaymentrequest=""
-                className={styles.donorbox}
-                height="900px"
-                name="donorbox"
-                seamless="seamless"
-                src="https://donorbox.org/embed/website-donation-15"
-                width="100%"
-              />
-            </GridItem>
-          </Grid>
-        </Grid>
+              <ul>
+                <li>
+                  <span className="fwb">$50</span> covers one mentee weekly
+                  learning stipend for our in-person program
+                </li>
+                <li>
+                  <span className="fwb">$150</span> covers meals and learning
+                  for 10 in-person mentees
+                </li>
+                <li>
+                  <span className="fwb">$300</span> covers one mentee learning
+                  stipend for our fall-intensive cohort
+                </li>
+                <li>
+                  <span className="fwb">$500</span> covers one mentee learning
+                  stipend for our 1:1 Technical Mentorship cohort
+                </li>
+              </ul>
+            </div>
+            <div className="twoUpCard">
+              <div className={cx(styles.donationWidgetWrapper)}>
+                <script
+                  src="https://donorbox.org/widget.js"
+                  paypalExpress="false"
+                />
+                <iframe
+                  title="Donate to Emergent Works!"
+                  allowpaymentrequest=""
+                  height="900px"
+                  name="donorbox"
+                  seamless="seamless"
+                  src="https://donorbox.org/embed/website-donation-15"
+                  width="100%"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </Layout>
   );
 };
-
-export const query = graphql`
-  {
-    Laptop: file(relativePath: { eq: "laptop.png" }) {
-      childImageSharp {
-        id
-        gatsbyImageData(layout: FULL_WIDTH)
-      }
-    }
-  }
-`;
 
 export default DonatePage;
